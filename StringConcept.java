7aacffb09f37787f89dc92ac4ffb3b46cd2c52b4
ankurcsscorp <48344394+ankurcsscorp@users.noreply.github.com>
2019-03-27 19:11:33 +0530

    I have to complite it

---
+++ StringConcept.java
@@ -0,0 +1,51 @@
+public class StringConcept
+
+{
+  
+  public static void main(String [] args)
+  
+  {
+ int count=1;
+ 
+       String st="abcd";
+   
+     String str[]={"donkey","cab","apple"};
+ 
+       char[] ch=st.toCharArray();
+    
+    do
+ 
+       {
+   
+     for(int i=0;i<ch.length;i++)
+  
+      {
+        
+    count++;
+     
+       for(int j=0;j<ch.length;j++)
+  
+      {
+     
+       String string=str[j];
+   
+         char[] chh=string.toCharArray();
+    
+        if(ch[i]==chh[0])
+    
+        {
+            
+    System.out.println(str[j]);
+
+            }
+     
+   }
+      
+  }
+   
+ }
+while(true);
+ 
+   }
+
+}
